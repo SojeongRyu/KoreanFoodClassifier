36f2e6d0778a777809a29694573592a7b6483857
sj_developer <thwjd703@naver.com>
2019-02-14 10:35:25 +0900

    add like button's event

--- KoreanFoodClassifier/app/src/main/java/com/example/sojeong/koreanfoodclassifier/ClickedRecipeActivity.java
+++ KoreanFoodClassifier/app/src/main/java/com/example/sojeong/koreanfoodclassifier/ClickedRecipeActivity.java
@@ -10,24 +10,37 @@ import android.widget.TextView;
 public class ClickedRecipeActivity extends Activity {
     Intent intent;
     String menu_name;
-    String test;
     TextView edit;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scrapped_recipe);
+
         intent = getIntent();
         menu_name = intent.getStringExtra("menu");
         edit = (TextView) this.findViewById(R.id.textView1);
         edit.append(menu_name);
 
         CheckBox liked = (CheckBox)findViewById(R.id.btn_selector);
-
+        liked.setOnClickListener(new CheckBox.OnClickListener(){
+            public void onClick(View v){
+                if(!((CheckBox)v).isChecked()) {
+                    Intent resultIntent = new Intent();
+                    resultIntent.putExtra("menu_name", menu_name);
+                    setResult(RESULT_OK, resultIntent);
+                }
+            }
+        });
 
     }
 
 
     public void onBackBtn2Clicked(View v) {
+       /* if(!liked.isChecked()){
+            Intent resultIntent = new Intent();
+            resultIntent.putExtra("menu_name", menu_name);
+            setResult(RESULT_OK, resultIntent);
+        }*/
         finish();
     }
 
